2b11c7f2fa03b9e0b9d6a9a73659fc0a39ebaced
Jan <jan@local>
2026-02-21 10:43:49 +0100

    Marlin: sync core to bed_preheat integration

--- lib/Marlin/Marlin/src/Marlin.cpp
+++ lib/Marlin/Marlin/src/Marlin.cpp
@@ -56,6 +56,7 @@
 #include "module/printcounter.h" // PrintCounter or Stopwatch
 #include "feature/closedloop.h"
 #include "feature/safety_timer.h"
+#include "feature/bed_preheat.hpp"
 #if !BOARD_IS_DWARF()
 #include "pause_stubbed.hpp"
 #endif
@@ -683,6 +684,10 @@ void idle(
   );
 
   thermalManager.manage_heater();
+
+  #if HAS_HEATED_BED
+    bed_preheat.update();
+  #endif
 
   #if ENABLED(PRINTCOUNTER)
     print_job_timer.tick();
